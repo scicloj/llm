ffa881f9a70ab6f92635d73e6f356d7055846641
Carsten Behring <carsten.behring@gmail.com>
2025-11-10 17:37:00 +0000

    use 'spinner' for progress
    comemnted

--- src/huggingface.clj
+++ src/huggingface.clj
@@ -4,31 +4,17 @@
    [clojure.java.io :refer [output-stream]]
    [clojure.string :as str]
    [clojure.java.io :as io]
+   [progress.determinate]
+   [jansi-clj.core]
    )
   (:import
    (org.apache.commons.io.input CountingInputStream)))
 
 
-
-(defn- print-progress-bar
-  "Render a simple progress bar given the progress and total. If the total is zero
-   the progress will run as indeterminated."
-  ([progress total] (print-progress-bar progress total {}))
-  ([progress total {:keys [bar-width]
-                    :or   {bar-width 50}}]
-   (if (pos? total)
-     (let [pct (/ progress total)
-           render-bar (fn []
-                        (let [bars (Math/floor (* pct bar-width))
-                              pad (- bar-width bars)]
-                          (str (str/join (repeat bars "="))
-                               (str/join (repeat pad " ")))))]
-       (print (str "[" (render-bar) "] "
-                   (int (* pct 100)) "% "
-                   progress "/" total)))
-     (let [render-bar (fn [] (str/join (repeat bar-width "-")))]
-       (print (str "[" (render-bar) "] "
-                   progress "/?"))))))
+(def spinner-style 
+  ;(:ascii-basic progress.determinate/styles)
+  (:coloured-ascii-boxes progress.determinate/styles)
+  )
 
 (defn- insert-at
   "Addes value into a vector at an specific index."
@@ -38,14 +24,6 @@
       (conj val)
       (into (subvec v idx))))
 
-(defn- insert-after
-  "Finds an item into a vector and adds val just after it.
-   If needle is not found, the input vector will be returned."
-  [v needle val]
-  (let [index (.indexOf v needle)]
-    (if (neg? index)
-      v
-      (insert-at v (inc index) val))))
 
 (defn- wrap-downloaded-bytes-counter
   "Middleware that provides an CountingInputStream wrapping the stream output"
@@ -57,7 +35,9 @@
       (merge resp {:body                     counter
                    :downloaded-bytes-counter counter}))))
 
-(defn- download-with-progress [url target authorization-token]
+
+
+(defn- download-with-progress [url target path authorization-token]
   (http/with-middleware
     (-> http/default-middleware
         ;(insert-after http/wrap-redirects wrap-downloaded-bytes-counter)
@@ -65,37 +45,39 @@
         (conj http/wrap-lower-case-headers))
     (let [response (http/get url
                              {:as :stream
-                              :headers {"Authorization" 
+                              :headers {"Authorization"
                                         (format "Bearer %s" authorization-token)}})
 
-          length (Long. (get-in response [:headers "content-length"] 0))
-          buffer-size (* 1024 1024)]
+          length (Integer. (get-in response [:headers "content-length"] Integer/MAX_VALUE))
+          buffer-size (* 1024 1024)
+          progress (atom 0)]
+
       
-      (println)
-      (with-open [input (:body response)
-                  output (output-stream target)]
-        (let [buffer (make-array Byte/TYPE buffer-size)
-              counter (:downloaded-bytes-counter response)]
-          
-          (loop [num-calls 0]
-            (let [size (.read input buffer)]
-              (when (pos? size)
-                (.write output buffer 0 size)
+      (progress.determinate/animate!
+       progress
+       :opts {:total (/ length 1024 1024)
+              :redraw-rate 60 ; Use 60 fps for the demo
+              :style spinner-style
+              :label path
+              :preserve true
+              :units "MB"
+              :line 3
+              }
+       (with-open [input (:body response)
+                   output (output-stream target)]
+         (let [buffer (make-array Byte/TYPE buffer-size)
+               counter (:downloaded-bytes-counter response)]
 
-                (when (or (zero? num-calls)
-                          (= 0 (mod num-calls 1000)))
-                  (print-progress-bar
-                   (if (some? counter)
-                     (Math/round (/ (.getByteCount counter) 1.0))
-                     0)
-                   (Math/round (/ length 1.0)))
-                  (println)
-                  )
-                (recur (inc num-calls)))))))
-      (println))))
+           (loop []
+             (let [size (.read input buffer)]
+               (when (pos? size)
+                 (.write output buffer 0 size)
+                 (reset! progress (/ (.getByteCount counter) 1024 1024))
+                 (recur)))))))
+      )))
 
 
-(defn download-onnx-model-dir! 
+(defn download-onnx-model-dir!
   "Download all files from a given 'model' from huggigface to a local dir"
 
   [models-base-dir model-name authorization-token]
@@ -103,28 +85,40 @@
         model-namespace (first split-by-slash)
         model-repo (second split-by-slash)
         model-base-dir (format "%s/%s/%s" models-base-dir model-namespace model-repo)
-        
+
         model-files
         (->
          (http/get (format "https://huggingface.co/api/models/%s/%s/tree/main?recursive=true" model-namespace model-repo)
                    {:headers {"Authorization" (format "Bearer %s" authorization-token)}
                     :as :json})
-         :body)]
-    (run!
-     (fn [{:keys [type path]}]
-       (case type
-         "directory" (io/make-parents (format "%s/%s" model-base-dir path))
-         "file"
-         (do
-           (io/make-parents (format "%s/%s" model-base-dir path))
-           (download-with-progress (format "https://huggingface.co/%s/%s/resolve/main/%s" model-namespace model-repo path)
-                                   (format "%s/%s" model-base-dir path)
-                                   authorization-token))))
-     model-files)))
+         :body)
+        progress (atom 0)]
+    (progress.determinate/animate!
+     progress 
+     :opts {:total (count model-files)
+            :line 2
+            :style spinner-style}
+     (run!
+      (fn [{:keys [type path]}]
+        (case type
+          "directory" (io/make-parents (format "%s/%s" model-base-dir path))
+          "file"
+          (do
+            (io/make-parents (format "%s/%s" model-base-dir path))
+            (download-with-progress (format "https://huggingface.co/%s/%s/resolve/main/%s" model-namespace model-repo path)
+                                    (format "%s/%s" model-base-dir path)
+                                    path
+                                    authorization-token)
+            (swap! progress inc)
+            )
+          ))
+      model-files))))
 
-(comment 
-  (download-onnx-model-dir!  "/tmp/models" 
-                             "onnx-community/text_summarization-ONNX" 
+(comment
+  (jansi-clj.core/erase-screen!)
+  
+  (download-onnx-model-dir!  "/tmp/models"
+                             "onnx-community/tiny-random-LlamaForCausalLM-ONNX"
                              "<token>")
   )  
 
