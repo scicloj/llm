4ef32dca441faa2a5011627414c1a212745df685
Carsten Behring <carsten.behring@efsa.europa.eu>
2025-12-14 14:00:16 +0000

    generation loop working
    - CPU only
    - no cache

--- src/inference_gemma3.clj
+++ src/inference_gemma3.clj
@@ -2,44 +2,53 @@
   (:require
    [tech.v3.datatype.argops]
    [uncomplicate.clojure-cpp
-    :refer [get-entry long-pointer pointer-pointer pointer-pointer
+    :refer [get-entry long-pointer pointer-pointer pointer-pointer put!
             short-pointer zero! float-pointer capacity! pointer-vec]]
    [uncomplicate.commons.core :as uc-co-core :refer [info with-release]]
-   [uncomplicate.diamond.internal.onnxrt.constants]
-   [uncomplicate.diamond.internal.onnxrt.core :refer [bound-values mutable-data value] :as onnxrt-internal]
+   [uncomplicate.diamond.internal.onnxrt.core :refer [clear-bound-outputs bound-values mutable-data value] :as onnxrt-internal]
    [uncomplicate.diamond.native]
    [fastmath.vector :as v])
   (:import
-   [ai.djl.huggingface.tokenizers HuggingFaceTokenizer]
-   [ai.onnxruntime.platform Fp16Conversions]))
+   [ai.djl.huggingface.tokenizers HuggingFaceTokenizer]))
 
-(def input-ids
-  [[2    105   2364    107   3048    659    496  11045  16326 236761
-    108   6974    786    496  27355   1003  15313  19180 236761    106
-    107    105   4368    107]])
+;(def model-base-dir "/media/xfs/hf-models")
 
-
-(def attention-mask [[1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]])
-
-(def batch-size (count input-ids))
 
 (def num-key-value-heads 1)
 
 (def head-dim 256)
 (def num-hidden-layers 26)
 
-(defn infer! []
-  (with-release [mem-info (onnxrt-internal/memory-info :cpu :arena 0 :default)
-                 env (onnxrt-internal/environment :warning "test" nil)
-                 opt (-> (onnxrt-internal/options)
-                         (onnxrt-internal/append-provider! :cuda)
-         ;(override-dimension! "batch_size" 1)
-                            ;(override-dimension! "sequence_length"  len-input)
-                            ;(override-dimension! "past_sequence_length" len-input)
-                            ;(override-dimension! "past_sequence_length + 1" (inc len-input))
-                            ;(graph-optimization! :extended)
-                         )
-                 sess (onnxrt-internal/session env "/hf-models/onnx-community/gemma-3-1b-it-ONNX/onnx/model.onnx" opt)
+(defn- ->last-token-id [data-binding sequence_length]
+  (let [logits-vec
+        (v/array-vec
+         (pointer-vec (capacity! (float-pointer (mutable-data (first (bound-values data-binding))))
+                                 (* sequence_length 262144))))
+        logits (partition 262144 logits-vec)
+
+        last-logit
+        (v/array-vec
+         (last logits))]
+
+    ;; (println :logits-vec (take 10 logits-vec))
+    ;; (println :first-bound-value (count logits-vec))
+    ;; (println first-five)
+    ;; (println last-five)
+
+    ;; (assert (v/edelta-eq  (v/array-vec [-17.110178 , -11.298628 ,   1.2429764, -18.418331 ,  -4.9682384])
+    ;;                       first-five
+    ;;                       0.001)
+    ;;         (str "unexpeced first-five: "  first-five))
+
+    ;; (assert (v/edelta-eq  (v/array-vec [-18.370398, -18.326473, -18.303009, -18.506184, -18.182766])
+    ;;                       last-five
+    ;;                       0.001)
+    ;;         (str "unexpeced last-five: "  last-five))
+
+    (v/maxdim last-logit)))
+
+(defn infer! [sess mem-info  input-ids batch-size]
+  (with-release [sequence_length (count (first input-ids))
 
                  past-key-values
                  (into {}
@@ -49,7 +58,7 @@
                           (onnxrt-internal/onnx-tensor mem-info [batch-size num-key-value-heads 0 head-dim]
                                                        (zero! (float-pointer 0)))]))
 
-                 len-input (-> input-ids first count)
+
                  present-key-values
                  (into {}
                        (for [layer (range  num-hidden-layers)
@@ -57,23 +66,23 @@
                          [(format "present.%s.%s" layer kv)
                           (onnxrt-internal/onnx-tensor mem-info [batch-size
                                                                  num-key-value-heads
-                                                                 len-input
+                                                                 sequence_length
                                                                  head-dim]
                                                        (zero! (float-pointer
                                                                (* batch-size
                                                                   num-key-value-heads
-                                                                  len-input
+                                                                  sequence_length
                                                                   head-dim))))]))
-                 position-ids (range 1 (inc len-input))
+                 position-ids (range 1 (inc sequence_length))
                  in-binding
                  (assoc past-key-values
                         "input_ids" (onnxrt-internal/onnx-tensor
                                      mem-info
-                                     [1 len-input]
+                                     [1 sequence_length]
                                      (long-pointer (-> input-ids first long-array)))
                         "position_ids" (onnxrt-internal/onnx-tensor
                                         mem-info
-                                        [1 len-input]
+                                        [1 sequence_length]
                                         (long-pointer (-> position-ids long-array))))
 
 
@@ -81,8 +90,8 @@
                  (assoc present-key-values
                         "logits" (onnxrt-internal/onnx-tensor
                                   mem-info
-                                  [1 len-input 262144]
-                                  (float-pointer (* len-input 262144))))
+                                  [1 sequence_length 262144]
+                                  (float-pointer (* sequence_length 262144))))
 
 
                  data-binding
@@ -91,39 +100,71 @@
 
                  next! (onnxrt-internal/runner* sess)]
 
+
     (next! data-binding)
 
 
-    (let [logits-vec
-          (v/array-vec
-           (pointer-vec (capacity! (float-pointer (mutable-data (first (bound-values data-binding))))
-                                   (* len-input 262144))))
-          last-logit
-          (v/array-vec
-           (last
-            (partition 262144 logits-vec)))
-          first-five (v/array-vec (take 5 last-logit))
-                 last-five (v/array-vec (take-last 5 last-logit))]
-          
-    (println :logits-vec (take 10 logits-vec))
-        
-    (println :first-bound-value (count logits-vec))
+    (->last-token-id data-binding sequence_length)))
 
-    (println first-five)
 
-    (println last-five)
-      
-      (assert (v/edelta-eq  (v/array-vec [-17.110178 , -11.298628 ,   1.2429764, -18.418331 ,  -4.9682384])
-                            first-five
-                            0.001)
-              (str "unexpeced first-five: "  first-five))
 
-      (assert (v/edelta-eq  (v/array-vec [-18.370398, -18.326473, -18.303009, -18.506184, -18.182766])
-                            last-five
-                            0.001)
-              (str "unexpeced last-five: "  last-five))
+(defn generate [ort-session mem-info initial-input-ids batch-size next-token-callback]
+  (reduce
+   (fn [accum _]
+     (let [next-token (infer! ort-session mem-info (:input-ids accum) batch-size)]
+       (next-token-callback {:token-id next-token})
+       {:input-ids [(conj (first (:input-ids accum)) next-token)]
+        :generated-tokens (conj (:generated-tokens accum) next-token)}))
+   {:input-ids initial-input-ids
+    :generated-tokens []}
+   (range 50)))
 
-      (println :next-token-id (v/maxdim last-logit))
-    (assert (= 19058 (v/maxdim last-logit))))))
 
-(infer!)
+(def model-base-dir  "/hf-models")
+(def model-name "onnx-community/gemma-3-1b-it-ONNX")
+(def tokenizer
+  (-> (format "file:%s/%s" model-base-dir model-name)
+      java.net.URI.
+      java.nio.file.Path/of
+      HuggingFaceTokenizer/newInstance))
+
+
+(def initial-input-ids [[2    105   2364    107   3048    659    496  11045  16326 236761
+                         108   6974    786    496  27355   1003  15313  19180 236761    106
+                         107    105   4368    107]])
+(def batch-size 1)
+
+(with-release [mem-info (onnxrt-internal/memory-info :cpu :arena 0 :default)
+               env (onnxrt-internal/environment :warning "test" nil)
+               opt (-> (onnxrt-internal/options)
+                       (onnxrt-internal/append-provider! :dnnl))
+               sess (onnxrt-internal/session env (format "%s/%s/onnx/model.onnx" model-base-dir model-name) opt)]
+  (generate sess mem-info initial-input-ids batch-size
+            (fn [next-token-info]
+              (print (.decode tokenizer (long-array [(:token-id next-token-info)])))
+              (flush))))
+
+
+
+;
+;; (println
+;;  (.decode tokenizer (long-array @generated-tokens)))
+
+
+(comment
+  (.decode tokenizer (long-array [1106 107 8623 108 659 496 11045 532 236761 107 19058
+                                  496 496 17856 1003 506 19180 107 108 249398 19058 70319 9366 19058]))
+
+
+
+  (def env (onnxrt-internal/environment :warning "test" nil))
+
+  (def opt
+    (-> (onnxrt-internal/options)
+        (onnxrt-internal/append-provider! :cuda)))
+
+
+  (def sess (onnxrt-internal/session env "/hf-models/onnx-community/gemma-3-1b-it-ONNX/onnx/model.onnx" opt)))
+
+
+
